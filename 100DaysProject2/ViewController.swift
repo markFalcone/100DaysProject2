4e8d624911b07ed0a727f143212092dac2abf021
Mark <markfalcone2@gmail.com>
2021-12-09 08:38:53 -0800

    Add game logic

--- 100DaysProject2/ViewController.swift
+++ 100DaysProject2/ViewController.swift
@@ -13,8 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
 
     override func viewDidLoad() {
@@ -42,11 +44,35 @@ class ViewController: UIViewController {
         askQuestion()
         
     }
-    
-    func askQuestion(){
+    @IBAction func ButtonTap(_ sender: UIButton) {
+        
+        var title: String
+        if sender.tag == correctAnswer{
+            title = "Correct"
+            score += 1
+        }else{
+            title = "Wrong"
+            score -= 1
+        }
+       
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+                     
+        
+        }
+
+    func askQuestion(action: UIAlertAction! = nil){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]),for: .normal)
         button2.setImage(UIImage(named: countries[1]),for: .normal)
         button3.setImage(UIImage(named: countries[2]),for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
     
     
