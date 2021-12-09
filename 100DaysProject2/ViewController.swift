bb606830339a615329151358c42d9d68ff4ff052
Mark <markfalcone2@gmail.com>
2021-12-09 08:09:57 -0800

    add border to image

--- 100DaysProject2/ViewController.swift
+++ 100DaysProject2/ViewController.swift
@@ -33,8 +33,25 @@ class ViewController: UIViewController {
 //        countries.append("uk")
 //        countries.append("us")
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        button1.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.gray.cgColor
+        button2.layer.borderWidth = 1
+        button2.layer.borderColor = UIColor.gray.cgColor
+        button3.layer.borderWidth = 1
+        button3.layer.borderColor = UIColor.gray.cgColor
+        askQuestion()
         
     }
+    
+    func askQuestion(){
+        button1.setImage(UIImage(named: countries[0]),for: .normal)
+        button2.setImage(UIImage(named: countries[1]),for: .normal)
+        button3.setImage(UIImage(named: countries[2]),for: .normal)
+    }
+    
+    
+    
+    
 
 
 }
